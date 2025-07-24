db1253315dbb598ab028c369388c04d21a5c073c
seoyeon-haru <sy5792@naver.com>
2025-07-24 09:48:05 +0900

    WIP : 다이얼 구현 중

--- lib/ui_0724.dart
+++ lib/ui_0724.dart
@@ -3,7 +3,45 @@ import 'package:flutter/material.dart';
 class Home0724 extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-   return Scaffold();
+   return Scaffold(
+    body: Column(
+      children: [
+        Container(),
+        Row(
+          children: [
+            Container(
+              width: 20,
+              height: 20,
+              decoration: BoxDecoration(borderRadius: BorderRadius.circular(30), color: Colors.grey),
+              child: Text('1'),
+            ),
+            Container(
+              width: 20,
+              height: 20,
+              decoration: BoxDecoration(borderRadius: BorderRadius.circular(30), color: Colors.grey),
+              child: Column(
+                children: [
+                  Text('2'),
+                  Text('A B C')
+                ],
+              ),
+            ),
+            Container(
+                   width: 20,
+              height: 20,
+              decoration: BoxDecoration(borderRadius: BorderRadius.circular(30), color: Colors.grey),
+              child: Column(
+                children: [
+                  Text('3'),
+                  Text('D E F')
+                ],
+              ),
+            )
+          ],
+        ),
+      ],
+    ),
+   );
   }
 
 }
